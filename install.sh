6d7da785aaca351e56b25cb1cf3091656973b357
Haogao Gu <koohoko@gmail.com>
2022-11-20 15:54:53 +0800

    default yes while install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install tmux
+sudo apt-get install -y tmux
 curl -LO https://github.com/ClementTsang/bottom/releases/download/0.6.8/bottom_0.6.8_amd64.deb
 sudo dpkg -i bottom_0.6.8_amd64.deb
 rm bottom_0.6.8_amd64.deb
